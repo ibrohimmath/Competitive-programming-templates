df92f88a602a95c3bba20aaddadddd29dc5b4754
ibrohimmath <ahmadjonovibrohim062@gmail.com>
2026-01-11 14:37:40 +0500

    feat: add fast I/O for python

--- python/main.py
+++ python/main.py
@@ -1,54 +1,24 @@
-# import sys
+import sys
 # sys.setrecursionlimit(100000)
 
 # from bisect import bisect_right
 
+inputln = sys.stdin.readline
+def println(*args, sep=' ', end='\n'):
+    sys.stdout.write(sep.join(map(str, args)) + end)
+
+inputTest = True
+# inputTest = False
 def solve(test_case: int) -> None:
-    N, M, X = map(int, input().split())
+    print('hello')
+    pass
 
-    events = []
-    for i in range(N):
-        a, b, v = map(int, input().split())
-        if a < b:
-            events.append([(X - b) / v, -1, i]) 
-            events.append([(X - a) / v, 1, i])
-        else:
-            events.append([(b - X) / v, -1, i])
-            events.append([(a - X) / v, 1, i])
-
-    temp = []
-    ans = [0] * M 
-    A = list(map(int, input().split()))
-    for t in range(M):
-        events.append([A[t], 2, t])
-
-    events.sort()
-    # print(events)
-
-    trains = 0
-    for time, type, ind in events:
-        if type == -1:
-            trains += 1
-        elif type == 1:
-            trains -= 1
-        else:
-            temp.append(ind)
-
-        if trains == 0:
-            for ind in temp:
-                ans[ind] = time
-            temp = []
-
-    print(*ans, sep='\n')
-
-inputTest = False
 def main():
     t = 1
     if inputTest:
-        t = int(input())
+        t = int(inputln())
 
     for i in range(1, t + 1):
         solve(i)
-
 
 main()
